6c805b7d34bf27f1b6b1d98a3ee8f9e2fd30a49c
Tim Ruthersby <truthe01@harris.com>
2020-11-08 13:07:46 -0700

    Inital Server created

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+const port = 3423;
+
+app.get('/:characterId', (req, res) => {
+    request(`https://dicecloud.com/character/${req.params.characterId}/json?key=${process.env.DICECLOUD_KEY}`).pipe(res);
+});
+
+
+app.listen(port);
