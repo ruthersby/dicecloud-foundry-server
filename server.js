8bdc28be89e34b89fc29c35c43117f94612cf1ae
Tim Ruthersby <truthe01@harris.com>
2020-11-08 13:16:40 -0700

    adding cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const request = require('request');
+var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3423;
+
+app.use(cors());
 
 app.get('/:characterId', (req, res) => {
     request(`https://dicecloud.com/character/${req.params.characterId}/json?key=${process.env.DICECLOUD_KEY}`).pipe(res);
 });
-
 
 app.listen(port);
